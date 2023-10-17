500963902874f7cce15aed89f2bcd1244563c2b3
Guil-levy <guil.levy12@gmail.com>
2023-10-17 19:43:52 +0300

    1

--- back-end/app.js
+++ back-end/app.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
 const knex = require('knex');
+
+//PORT SERVER
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
