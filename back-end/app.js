a292a90125b02582a72190ce0fc2c42f9a9f05f5
Guil-levy <guil.levy12@gmail.com>
2023-10-18 21:41:10 +0300

    1

--- back-end/app.js
+++ back-end/app.js
@@ -1,9 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const knex = require('knex');
+const knex = require("knex");
+const axios = require("axios");
+const db = require("./db");
+
+//
+app.get('/db', (req, res) => {
+  // Handle the route logic here
+});
+// --------------------
 
 //PORT SERVER
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+  console.log(`Server is running on port ${port}`);
+});
--- back-end/db.js
+++ back-end/db.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
 const knex = require('knex');
+
+//CONNECTION WITH PG
 const db = knex({
     client: 'pg',
     connection: {
@@ -10,53 +12,58 @@ const db = knex({
       database : 'uxmohmcw',
     },
   });
+
   //URLS
 // const imageUrl1 = 'https://vignette.wikia.nocookie.net/starwars/images/4/49/Lukeportrait.jpg/revision/latest?cb=20151221055502&path-prefix=ro';
 const imageUrl2 = 'https://wallpapercave.com/wp/wp5825056.jpg';
 
 
-//INSERT
-axios.get(imageUrl2, { responseType: 'arraybuffer' })
-  .then(response => {
-    // Convert binary image data to hexadecimal
-    const binaryImageData2 = Buffer.from(response.data).toString('hex');
-    // Insert the image data into the database
-    db("characters")
-      .insert({
-        username: "Darth Vader",
-        image_column: binaryImageData2
-      })
-      .then(data => {
-        console.log("Image data inserted successfully");
-      })
-      .catch(err => {
-        console.error("Error inserting image data:", err);
-      });
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+//INSERTS
+// axios.get(imageUrl2, { responseType: 'arraybuffer' })
+//   .then(response => {
+//     // Convert binary image data to hexadecimal
+//     const binaryImageData2 = Buffer.from(response.data).toString('hex');
+//     // Insert the image data into the database
+//     db("characters")
+//       .insert({
+//         username: "Darth Vader",
+//         image_column: binaryImageData2
+//       })
+//       .then(data => {
+//         console.log("Image data inserted successfully");
+//       })
+//       .catch(err => {
+//         console.error("Error inserting image data:", err);
+//       });
+//   })
+//   .catch(error => {
+//     console.error('Error:', error);
+//   });
 
-// //select
-// db.raw("select *from characters")
-// db.select("*")
-// .from("characters")
-// .then((rows) =>{
-//     console.log("rows=>",rows)
-// })
-// .catch((e) =>{
-//  console.log(e);
-// });
+//SELECT
+db.raw("select *from characters")
+db.select("*")
+.from("characters")
+.then((rows) =>{
+    console.log("rows=>",rows)
+})
+.catch((e) =>{
+ console.log(e);
+});
+
+
 //  ////select one item
-// // db.raw("select * from products where id = 1").then((data) =>{
+// // db.raw("select * from characters where id = 1").then((data) =>{
 // //     console.log("rows=>", data.rows);
 // // })
+
 // ////select two items
-// // db.raw("select * from products where id >= ?",[2]).then((data) =>{
+// // db.raw("select * from characters where id >= ?",[2]).then((data) =>{
 // //     console.log("rows=>", data.rows);
 // // })
+
 // ////delete
-// // db("products")
+// // db("characters")
 // // .where({id:3})
 // // .del()
 // // .returning(["id","price","name"])
@@ -66,20 +73,10 @@ axios.get(imageUrl2, { responseType: 'arraybuffer' })
 // // .catch((err) =>{
 // //     console.log(err);
 // // });
-// //insert
-// db("characters ")
-// .insert([
-//     {username:"Luke Skywalker",},
-//     {image_column:"binaryImageData1", }
-// ])
-// .then((data) =>{
-//     console.log(data);
-// })
-// .catch((err) =>{
-//     console.log(err);
-// });
-// ///updata
-// // db("products")
+
+
+// ///update
+// // db("characters")
 // // .update({ price:444})
 // // .where({id:3})
 // // .then((data) =>{
