8cab10e870851657c3d9aa96a05f1f854881ac3a
Guil-levy <guil.levy12@gmail.com>
2023-10-19 20:09:16 +0300

    1

--- front-end/script.js
+++ front-end/script.js
@@ -1,8 +1,21 @@
+function displayImages() {
 fetch("http://localhost:3000/db")
     .then((data)=>data.json())
-    .then((data)=>console.log(data))
-    .catch((e)=>console.log(e))
-
-array.forEach(element => {
+    .then(data => {
+        const imageGallery = document.getElementById('imageGallery');
         
-});
+        data.forEach(character => {
+            const imageUrl = character.image_url;
+            
+            // Create an image element
+            const img = document.createElement('img');
+            img.src = imageUrl;
+            
+            // Append the image element 
+            imageGallery.appendChild(img);
+        });
+    })
+    .catch((err)=>console.log(err))
+;
+}
+displayImages();
